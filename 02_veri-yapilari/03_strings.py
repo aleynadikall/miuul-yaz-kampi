1adb63a40769e7b1700c9b034bc63a1ea64b1ade
Aleyna Dikal <aleyynadikal@gmail.com>
2022-07-04 03:46:53 +0300

    Karakter Dizileri

---
+++ 02_veri-yapilari/03_strings.py
@@ -0,0 +1,12 @@
+# Karakter Dizileri (Strings)
+
+print("John")
+
+# Print olmadan dabu ifadeleri çalıştırabiliriz.
+"John"   # yazmamız bunun için yeterlidir.
+
+# DİKKAT: Bir program veya fonksiyon yazıyorsak bu program veya fonksiyonun içerisindeki bir noktada ekrana bir bilgi
+# paylaşmak istiyorsak ancak bu durumda print kullanmamız gerekir. Etkileşimden yararlanmak istediğimizde ise ipython'ın
+# bize sağladığı bu etkileşimden yararlanırken daha genel olarak aslında print yazmadan kullanıyor olacağız. Fakat
+# fonksiyonlar ys da döngülere geçtiğimizde ekranda bir bilgi paylaşmak istediğimizdebu durumlarda printi sık bir şekilde
+# kullanacağız.
