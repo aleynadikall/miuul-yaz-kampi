3b1709dcece9b95ea394abb747f7d96f0ed46ec6
Aleyna Dikal <aleyynadikal@gmail.com>
2022-07-04 03:34:50 +0300

    Sayılar (Numbers)

---
+++ 02_veri-yapilari/02_sayilar.py
@@ -0,0 +1,24 @@
+# SAYILAR (NUMBERS)
+
+a = 5
+b = 10.5
+
+# atama yaptık ancak çağırma yapmadık bu yüzden ekrana basılmıyor.
+a # yazarsak ekrana basmış oluruz.
+
+
+a * 3
+a / 7
+a * b / 10
+
+# Kare alma işlemi
+a ** 2
+
+# Tip Değiştirme:
+int(b)      # 10
+float(a)    # 5.0
+
+int(a * b / 10)
+
+c = a * b / 10
+int(c)
