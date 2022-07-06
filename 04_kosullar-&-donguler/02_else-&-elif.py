745ba2a78c3c65f101999c4e404c04aa56764b6f
Aleyna Dikal <aleyynadikal@gmail.com>
2022-07-06 10:30:06 +0300

    Else & Elif

---
+++ 04_kosullar-&-donguler/02_else-&-elif.py
@@ -0,0 +1,32 @@
+# ELSE
+
+def number_check(number):
+    if number == 10:
+        print("number is 10")
+    else:
+        print("number is not 10")
+
+
+number_check(12)        # number is not 10
+
+
+def number_checker(number):
+    if number > 10:
+        print("greater than 10")
+    elif number < 10:
+        print("less than 10")
+    else:
+        print("equal to 10")
+
+
+number_checker(10)      # equal to 10
+
+
+
+
+
+
+
+
+
+
