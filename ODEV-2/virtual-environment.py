414ad5ad8d5308b1bc63bd337d238e6416fea9a0
Aleyna Dikal <aleyynadikal@gmail.com>
2022-07-07 22:29:57 +0300

    Virtual Environment Oluşturma

---
+++ ODEV-2/virtual-environment.py
@@ -0,0 +1,26 @@
+# ODEV 2: Virtual Environment Oluşturma
+
+# Görev 1: Kendi isminizde bir virtual environment oluşturunuz, oluşturma esnasında python 3 kurulumu yapınız.
+# Görev 2: Oluşturduğunuz environment'ı aktif ediniz.
+# Görev 3: Yüklü paketleri listeleyiniz.
+# Görev 4: Environment içerisine Numpy'ın güncel versiyonunu ve Pandas'ın 1.2.1 versiyonunu aynı anda indiriniz.
+# Görev 5: İndirilen Numpy'ın versiyonu nedir?
+# Görev 6: Pandas'ı upgrade ediniz. Yeni versiyonu nedir?
+# Görev 7: Numpy'ı environment'tan siliniz.
+# Görev 8: Seaborn ve matplotlib kütüphanesinin güncel versiyonlarını aynı anda indiriniz.
+# Görev 9: Virtual environment içindeki kütüphaneleri versiyon bilgisi ile beraber export ediniz ve yaml dosyasını
+# inceleyiniz.
+# Görev 10: Oluşturduğunuz environment'i siliniz. Önce environment'i deactivate ediniz.
+
+# Görev 1: conda create -n aleynadikall
+# Görev 2: conda activate aleynadikall
+# Görev 3: conda env list
+# Görev 4: conda install numpy pandas==1.2.1
+# Görev 5: İndirilen Numpy'ın versiyonu nedir? (conda list olabilir)
+# Görev 6: conda upgrade pandas,  Yeni versiyonu nedir?
+# Görev 7: Numpy'ı environment'tan siliniz.
+# Görev 8: Seaborn ve matplotlib kütüphanesinin güncel versiyonlarını aynı anda indiriniz.
+# Görev 9: Virtual environment içindeki kütüphaneleri versiyon bilgisi ile beraber export ediniz ve yaml dosyasını
+# inceleyiniz.
+# Görev 10: Oluşturduğunuz environment'i siliniz. Önce environment'i deactivate ediniz.
+
