14775a51347787d03ca1184af0617f28c49a5010
Aleyna Dikal <aleyynadikal@gmail.com>
2022-07-07 22:49:15 +0300

    Python ile Veri Analizine Giriş

---
+++ 06_python-ile-veri-analizi-numpy/01_python-ile-veri-analizine-giris.py
@@ -0,0 +1,12 @@
+# Python ile veri analizi (Data Analysis With Python)
+
+# - Numpy
+# - Pandas
+# - Veri Görselleştirme: Matplotlib & Seaborn
+# - Gelişmiş Fonksiyonel Keşifçi Veri Analizi (Advanced Functional Exploratory Data Analysis)
+
+
+
+
+
+
