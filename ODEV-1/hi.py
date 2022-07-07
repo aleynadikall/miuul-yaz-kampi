259006433be06ca8b5756601b67141fb1fd69fb7
Aleyna Dikal <aleyynadikal@gmail.com>
2022-07-07 22:07:16 +0300

    Ödev 1

---
+++ ODEV-1/hi.py
@@ -0,0 +1,16 @@
+# ODEV 1
+
+print("Merhaba, ben Aleyna Dikal")
+
+
+# Görev1: PyCharm'da "hi.py" isminde python dosyası oluşturunuz.
+# Görev2: Bu dosyanın içirisine şu kodu kendinize göre düzenleyiniz ve kaydediniz.
+# Görev3: Konsoldan üzerinden "hi.py" dosyasının olduğu dizine (klasöre) gidiniz. PyCharm’da sol tarafta yer alan
+# menüde hi.py dosyası hangi klasördeyse o klasöre sağ tuş ile tıklayıp şu seçimi yapınız: "open in > terminal".
+# PyCharm'ın alt tarafında terminal ekranı açılacak. Şu anda hi.py dosyası ile aynı dizindesiniz (klasörde).
+# Görev4: Konsola python hi.py yazarak, python kodunuzu çalıştırınız.
+
+
+
+
+
