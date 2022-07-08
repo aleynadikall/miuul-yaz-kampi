b72d8e55fa24d1ee90f0d157583b4d94862d20df
Aleyna Dikal <aleyynadikal@gmail.com>
2022-07-08 23:14:53 +0300

    Pandas Giriş

---
+++ 07_python-ile-veri-analizi-pandas/01_giris.py
@@ -0,0 +1,19 @@
+# PANDAS
+
+# Veri manipülasyonu ya da veri analizi dendiğinde akla gelen ilk python kütüphanelerinden birisidir.
+# Öncelikle ekonometrik ve finansal çalışmalar için doğmuş daha sonra veri analitiği dendiğinde en sık kullanılan
+# kütüphane haline gelmiştir.
+# Temeli 2008 yılında atılmıştır.
+# Birçok veri yapısı ile çalışma imkanı sağlamaktadır.
+# Veri analitiği genel başlığı altında makine öğrenmesinden veri bilimine, veri analizinden derin öğrenmeye, veri varsa
+# ve python varsa bir şekilde mutlaka elimizin dokunacağı en önemli kütüphanelerden birisidir.
+
+# Pandas Series
+# Veri Okuma (Reading Data)
+# Veriye Hızlı Bakış (Quick Look at Data)
+# Pandas'ta Seçim İşlemleri (Selection in Pandas)
+# Toplulaştırma ve Gruplama (Aggregation & Grouping)
+# Apply ve Lambda
+# Birleştirme (Join) İşlemleri
+
+
